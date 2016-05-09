ab48e6e2d969235ab6cf604ed6f61efaa6807256
Matt Boubin <boubinmj@miamioh.edu>
2016-05-08 21:40:14 -0400

    more pictures

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,12 +1,6 @@
 // transmitter.pde
 //
 // Simple example of how to use VirtualWire to transmit messages
-// Implements a simplex (one-way) transmitter with an TX-C1 module
-//
-// See VirtualWire.h for detailed API docs
-// Author: Mike McCauley (mikem@airspayce.com)
-// Copyright (C) 2008 Mike McCauley
-// $Id: transmitter.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
 
 #include <VirtualWire.h>
 
